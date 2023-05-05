b0cc427ce4ea259b296f6daa4fdc0d82ce12fe9d
cleverprogrammer <cleverprogrammer.com@gmail.com>
2023-05-05 18:33:58 +0100

    done the hole assignment project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Homepage from "./Homepage";
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
