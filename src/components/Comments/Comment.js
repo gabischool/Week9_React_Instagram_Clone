43c13bf6253135d8ba7869074d9a1492d239afe0
Yaasiin Ahmed <109895325+yaasiinaxmed@users.noreply.github.com>
2023-05-05 14:50:05 +0300

    waan kashaqey Assignment-ga

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -4,6 +4,8 @@ import React from 'react';
 const Comment = props => {
   // 🔥 Make sure the parent of Comment is passing the right props!
   // Hubi in component-ga ka sareysa midaan ay props sax ah soo direyso.
+  console.log(props.comment);
+
   const { comment } = props;
 
   return (
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,12 +5,19 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   // Hubi in component-ga ka sareysa midaan ay props sax ah soo direyso.
-  const { comments } = props;
+  console.log(props.Comments);
+
+  const { Comments } = props;
 
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
       {/* Dulmar prop-ka 'comments', kadib soo celi 'Comment' component, adigoo waxa ay 'Comment' u baahantahay u diraayo. */}
+      
+      {Comments.map(comment => {
+        return <Comment key={comment.id} comment={comment}/>
+      })}
+
     </div>
   );
 };
