ac2543b92799b2cc88c3013d78589f128e286775
Hani Nuur Bocor <116567403+Hanaafilsan@users.noreply.github.com>
2023-06-02 17:53:19 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+/* 
+  Start here and work your way down the nested components.
+  Not all files in the project need code added.
+  Look at each file to see what props need to be passed!
+*/
+
+/* 
+  Halkaan ka bilow, components-ka hoos usii gal.
+  Qasab ma aha in file-kasta oo project-gaan ku jiro inaa wax ka badasho.
+  File-kasta fiiri si aad u ogaato PROPS-ka uu doonaayo.
+*/
+
+import React, {useState} from 'react';
+// Import the Posts (plural!) and SearchBar components, since they are used inside App component
+// Soo jiido Posts iyo SearchBar components-ka
+import SearchBar from "./components/SearchBar/SearchBar";
+import Posts from './components/Posts/Posts';
+import dummyData from './dummy-data';
+
+// Import the dummyData
+// Soo jiido dummyData
+
+
+import './App.css';
+
+const App = () => {
+  // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+  // Waxaa sameysaa state la dhaho 'posts' si aad ugu keydisid wixii dummyData ka imaanaayo.
+  const [searchBar , setSearchBar] = useState(dummyData);
+  const [posts , setPosts] = useState(dummyData);
+  // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  // Hadaa rabtid inaad "SearchBar" ka shaqeysiisid (Waa Stretch ogow), waxaan u baahanahay state kale oo aad ku keydisid qoraalka aad raadineysid.
+  return (
+    <div className='App'>
+      {/* Add SearchBar and Posts here to render them */}
+      {/* Soo jiido SearchBar iyo Posts components-ka hoos, si aad u muujiyo */}
+      <SearchBar searchBar={searchBar} setSearchBar={setSearchBar}/>
+      <Posts posts={posts} setPosts={setPosts}/>
+      {/* Check the implementation of each component, to see what props they require, if any! */}
+      {/* Fiiri sida loo sameeyo component-kasta, si aad u ogaato PROPS-ka ay u baahan tahay, hadayba jiraan props ay u baahanatahy! */}
+    </div>
+  );
+};
+
+export default App;
